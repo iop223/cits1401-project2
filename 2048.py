b162c4be9ba4499fdf402e41f217a83ecf094f03
iop223 <dfens223@gmail.com>
2016-11-03 22:36:32 +0800

    assbergers

--- 2048.py
+++ 2048.py
@@ -18,10 +18,17 @@ def erase(win):
     rect = Rectangle(Point(Xmin,Xmin), Point(Xmax,Ymax))
     rect.setFill(color_rgb(139,69,19))
     rect.draw(win)
+def drawboxes(win):
+    for i in range(0,5):
+        for j in range(0,5):
+            box = Rectangle(Point(Xmin+20+i*5,Xmax+20+j*5), Point(Xmin+50+j*5,Ymax+50+j*5))
+            box.setFill(color_rgb(255,0,255))
+            box.draw(win)
 
 #This function should make the Graphical User Interface i.e. the Entry boxes and the Buttons.
 #It should return the variable identifiers of the Buttons and Entry boxes to the calling function. 
-def makeInterface(win): #4 marks
-    win.setBackground(color_rgb(238,0,238))
+def makeInterface(win):
+    win.setBackground(color_rgb(139,69,19))
     erase(win)
+    drawboxes(win)
 main()
