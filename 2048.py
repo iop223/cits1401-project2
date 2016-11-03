53c07a46628d802c3e40ecdad1f3b1a54d8ac688
iop223 <dfens223@gmail.com>
2016-11-04 00:06:00 +0800

    buttons

--- 2048.py
+++ 2048.py
@@ -6,12 +6,15 @@ Xmin, Xmax = 0, 720
 Ymin, Ymax = 0, 720
 
 #This function is the main control of the flow of the program
-#It draws dem boxes
 def main():
     win = GraphWin('wewuz', Xmax-Xmin, Ymax-Ymin)    
     win.setCoords(Xmin, Ymin, Xmax, Ymax)
-    makeInterface(win)
+    newGamebutton, quitButton = makeInterface(win)
     x = 0
+    while x==0:
+        Pt = win.getMouse()
+        if isClicked(Pt,quitButton):
+            win.close
     
 
 def erase(win):
@@ -31,4 +34,31 @@ def makeInterface(win):
     win.setBackground(color_rgb(139,69,19))
     erase(win)
     drawboxes(win)
+    scoreBoard(win)
+
+    #rectangle button with text
+    quitButton = Rectangle(Point(Xmax-100, Ymax-50),Point(Xmax, Ymax))
+    quitButton.setFill('#568203')
+    quitButton.draw(win)
+    q = Text(Point(Xmax-50, Ymax-25), "Quit")
+    q.draw(win)
+
+    #rectangle button with text
+    newGamebutton = Rectangle(Point(Xmax-100, Ymax-110),Point(Xmax, Ymax-60))
+    newGamebutton.setFill('#568203')
+    newGamebutton.draw(win)
+    ng = Text(Point(Xmax-50, Ymax-85), "New Game")
+    ng.draw(win)
+
+
+#This function should take the Point pClick where the mouse was clicked and the button identifier and return True if the click was inside the button otherwise it should return False.
+def isClicked(pClick, button):
+    if (pClick.getX() > button.getP1().getX()) and (pClick.getY() > button.getP1().getY()) and (pClick.getX() < button.getP2().getX()) and (pClick.getY() < button.getP2().getY()):
+        return True
+
+
+#This function Displays the score
+def scoreBoard(win):
+    scoreDisplay = Text(Point(Xmin+50,Ymin+50), "fag").draw(win)
+
 main()
